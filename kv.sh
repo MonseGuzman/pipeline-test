77a8e83e7efd766c32579a7699340e3d85f1f931
MonseGuzman <andreaguzman.lopez23@gmail.com>
2020-07-15 18:52:35 -0500

    install consul in script

--- kv.sh
+++ kv.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+# install consul
+curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
+
+sudo apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"sudo apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
+
+sudo apt-get update && sudo apt-get install consul
+
+# use consul
 export CONSUL_HTTP_ADDR=127.0.0.1:8500
 
 consul join -h
