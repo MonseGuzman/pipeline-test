47ba02053a21da8bd59922016837070c3d712669
Monse Guzmán <andreaguzman.lopez23@gmail.com>
2020-10-16 13:11:44 -0500

    Test pr (#2)
    * Update config.yml
    
    * do you create a release?
    
    * renamme version
    
    * rename step
    
    * hey adding the powerful export
    
    * look! new changes
    
    * update config.yml
    
    * Fix files
    
    * ops
    
    * final test
    
    * what happen if...
    
    * i hate you
    
    * i hate you x2
    
    * i forget it
    
    * hey
    
    * check the release (again)
    
    * a lot of text
    
    * test
    
    * testing
    
    * Test on app
    
    * ops
    
    * TEST

--- version-git.sh
+++ version-git.sh
@@ -1,38 +1,41 @@
 #!/bin/bash
 
 # read last version
-lastVersion=$(cat VERSION)
-# delete VERSION file
-rm VERSION
-# my variables
-arrayVersion=();
-change=false
-IFS='.' read -ra ADDR <<< "$lastVersion"
-for x in "${ADDR[@]}" 
-do
-    # Is it a number? 
-    if [[ "$x" =~ ^[0-9]+$ ]]; then 
-        # only for change one number
-        if [ "$change" = false ]; then
-            # x <= 9 (any number)
-            if [ "$x" -lt 99 ]; then
-                new_vs=`expr $x + 1`;
-                # invert the number
-                arrayVersion+=.$(echo $new_vs | rev);
-                # change the variable to avoid change another number
-                change=true
+lastVersion=$(git tag | sort -V | tail -1)
+# is it empty?
+if [ -z "$lastVersion" ]; then
+    echo "0.0.1"
+else
+    # invert the version
+    revLastVersion=$(echo $lastVersion | rev)
+    # my variables
+    arrayVersion=();
+    change=false
+    IFS='.' read -ra ADDR <<< "$revLastVersion"
+    for x in "${ADDR[@]}" 
+    do
+        # Is it a number? 
+        if [[ "$x" =~ ^[0-9]+$ ]]; then
+            # only for change one number
+            if [ "$change" = false ]; then
+                # x <= 9 (any number)
+                if [ "$x" -lt 99 ]; then
+                    revNum=$(echo $x | rev)
+                    new_vs=`expr $revNum + 1`;
+                    # invert the number
+                    arrayVersion+=.$(echo $new_vs | rev);
+                    # change the variable to avoid change another number
+                    change=true
+                else
+                    arrayVersion+=(.0)
+                fi
             else
-                arrayVersion+=(.0)
+                arrayVersion+=.$x
             fi
-        else
-            arrayVersion+=.$x
         fi
-    fi
-done
-
-# save the new version
-echo $arrayVersion >> VERSION
-# invert the version
-rev=$(echo $arrayVersion | rev)
-# final version
-echo "${rev%?}"
+    done
+    # invert the version
+    rev=$(echo $arrayVersion | rev)
+    # final version
+    echo "${rev%?}"
+fi
